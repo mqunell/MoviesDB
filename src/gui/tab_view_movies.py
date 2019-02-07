509ae291b38471360cd34bba95fbef9ebc05a498
Matt <mqunell@gmail.com>
2019-02-07 01:58:37 -0800

    Fix row elements that displayed incorrectly

--- src/gui/tab_view_movies.py
+++ src/gui/tab_view_movies.py
@@ -1,6 +1,8 @@
 from PyQt5.QtWidgets import *
-from PyQt5.QtCore import pyqtSlot, Qt
+from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QStandardItemModel
+from PyQt5.QtWidgets import QTreeView
+
 from src.db_connection import DbConnection
 
 
@@ -19,31 +21,63 @@ class TabViewMovies(QWidget):
 
         movies = DbConnection().get_movies()
 
-        model = self.create_model(self)
-        movies_view.setModel(model)
+        table = self.create_table(self)
+        movies_view.setModel(table)
+
+        # Translations for database data -> readable data
+        self.ratings = {1: 'G', 2: 'PG', 3: 'PG-13', 4: 'R', 5: 'Unrated'}
+        self.formats = {1: 'DVD', 2: 'Bluray', 3: '4K Bluray', 4: 'Hard Drive', 5: 'Streaming'}
 
         for movie in movies:
-            self.add_movie(model, movie)
+            self.add_movie(table, movie)
 
 
-    def create_model(self, parent):
+    def create_table(self, parent):
+        """
+        Creates and sets up the table.
+
+        :param parent: The parent widget (this class).
+        :return: The set up table.
+        """
+
         headers = ['Title', 'Year', 'Rating', 'Runtime', 'Genre', 'Director', 'Actors', 'Plot', 'PosterLink',
                    'Metacritic', 'SeriesName', 'SeriesNumber', 'Format']
 
-        model = QStandardItemModel(0, len(headers), parent)
+        table = QStandardItemModel(0, len(headers), parent)
 
         for i in range(0, len(headers)):
-            model.setHeaderData(i, Qt.Horizontal, headers[i])
+            table.setHeaderData(i, Qt.Horizontal, headers[i])
 
-        return model
+        return table
 
 
-    def add_movie(self, model, movie):
-        dict_keys = list(movie)
+    def add_movie(self, table, movie):
+        """
+        Creates and fills a row in the table for a movie.
 
-        current_row = model.rowCount()
+        :param table: The table to be added to.
+        :param movie: A dictionary representing a movie from the database.
+        """
 
-        model.insertRow(current_row)
+        current_row = table.rowCount()
 
-        for i in range(0, len(movie)):
-            model.setData(model.index(current_row, i), movie[dict_keys[i]])
+        table.insertRow(current_row)
+
+        # Add elements to the row, adjusting them as necessary
+        table.setData(table.index(current_row, 0), movie['Title'])
+        table.setData(table.index(current_row, 1), movie['Year'])
+        table.setData(table.index(current_row, 2), self.ratings[movie['Rating']])
+        table.setData(table.index(current_row, 3), str(movie['Runtime'])[:4])
+        table.setData(table.index(current_row, 4), movie['Genre'])
+        table.setData(table.index(current_row, 5), movie['Director'])
+        table.setData(table.index(current_row, 6), movie['Actors'])
+        table.setData(table.index(current_row, 7), movie['Plot'])
+        table.setData(table.index(current_row, 8), movie['PosterLink'])
+        table.setData(table.index(current_row, 9), movie['Metacritic'] if movie['Metacritic'] != -1 else '')
+        table.setData(table.index(current_row, 10), movie['SeriesName'])
+        table.setData(table.index(current_row, 11), movie['SeriesNumber'])
+
+        formats = ''
+        for digit in movie['Format']:
+            formats += self.formats[int(digit)] + ', '
+        table.setData(table.index(current_row, 12), formats[:-2])
